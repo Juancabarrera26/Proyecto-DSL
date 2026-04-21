dd4a052d0ea85c0611354c727b741c0d319dd555
ProyectCyber <108159807+Juancabarrera26@users.noreply.github.com>
2026-04-21 18:46:20 -0500

    Add files via upload

---
+++ generated/DeepLangLexer.py
@@ -0,0 +1,5 @@
+# Este archivo es generado automaticamente por ANTLR4.
+# No editar manualmente.
+#
+# Para regenerarlo ejecutar desde la raiz del proyecto:
+#   java -jar antlr4.jar -Dlanguage=Python3 -visitor -o generated DeepLang.g4
